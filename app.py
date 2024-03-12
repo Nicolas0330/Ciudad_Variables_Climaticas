ef11b0b20907f467e4bce30700592ae1878de11b
Nicolas0330 <163177274+Nicolas0330@users.noreply.github.com>
2024-03-12 14:18:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ st.header("En este espacio podras obtener informacion sobre variables climaticas
 
 image = Image.open('Clima.jpg')
 st.image(image, caption='Inteligencia Urbana')
-# st.write("Enlace para mi sistema de internet de las cosas")
-# st.write("Ingresa al link")
+st.write("Enlace para mi sistema de internet de las cosas")
+st.write("Ingresa al link [link] (http://demo.thingsboard.io/dashboards/9a61e170-565c-11ee-b6bf-f9525dc44ab3)")
