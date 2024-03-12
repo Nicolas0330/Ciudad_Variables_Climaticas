49d23049f277faf4c47879b119b4052f28352322
Nicolas0330 <163177274+Nicolas0330@users.noreply.github.com>
2024-03-12 14:10:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Aplicaciones para Ciudades Inteligentes y Sostenibles")
 
 st.header("En este espacio podras obtener informacion sobre variables climaticas en ciudades alrededor del mundo")
 
-image = Image.open('Escultura.jpg')
+image = Image.open('Clima.jpg')
 st.image(image, caption='Inteligencia Urbana')
 # st.write("Enlace para mi sistema de internet de las cosas")
 # st.write("Ingresa al link")
